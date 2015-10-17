8b87ca9cad6d44a7c147482f9cdf52c9f28a3696
Aidan Musnitzky <musnit@gmail.com>
2015-10-17 17:53:18 +0200

    Move all logic/popup into plugin and return oauth strings

--- src/ios/DigitsCordova.m
+++ src/ios/DigitsCordova.m
@@ -5,10 +5,35 @@
 
 - (void)openDigitsView:(CDVInvokedUrlCommand*)command
 {
-    self.command = command;
 
     [self.commandDelegate runInBackground:^{
-        [self.viewController performSegueWithIdentifier: @"digitize" sender: self];
+        self.command = command;
+        
+        [[Digits sharedInstance] authenticateWithCompletion:^
+         (DGTSession* session, NSError *error) {
+             
+             CDVPluginResult* pluginResult = nil;
+             
+             if (session) {
+                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+             } else {
+                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+             }
+
+             DGTOAuthSigning *oauthSigning = [[DGTOAuthSigning alloc] initWithAuthConfig:[Digits sharedInstance].authConfig authSession:session];
+             NSDictionary *authHeaders = [oauthSigning OAuthEchoHeadersToVerifyCredentials];
+             NSDictionary *loginResponse = [NSDictionary dictionaryWithObjectsAndKeys:
+                                            authHeaders, @"oauth_echo_headers",
+                                            nil];
+             
+             CDVPluginResult* result = [CDVPluginResult
+                                        resultWithStatus:CDVCommandStatus_OK
+                                        messageAsDictionary:loginResponse
+                                        ];
+             
+             [self success:result callbackId:command.callbackId];
+             
+         }];
     }];
 
 }
