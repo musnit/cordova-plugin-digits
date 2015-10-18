691c946a8d175563e263419a78b985c165525643
Aidan Musnitzky <musnit@gmail.com>
2015-10-19 00:26:01 +0200

    fix up clobbers

--- www/digits.js
+++ www/digits.js
@@ -1,7 +1,12 @@
-/*global cordova, module*/
+var exec = require('cordova/exec');
+
+var noop = function() {};
 
 module.exports = {
   openDigitsView: function (name, successCallback, errorCallback) {
+      successCallback = successCallback || noop;
+      errorCallback = errorCallback || noop;
+
       cordova.exec(successCallback, errorCallback, "Hello", "openDigitsView");
   }
 };
